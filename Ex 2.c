581104bfb25a5b9881faf6b71b163d83c3c718c4
Santhosh Raj Adabala <92911013+Santhosh396@users.noreply.github.com>
2021-10-23 11:58:00 +0530

    Add files via upload

---
+++ Ex 2.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("S\n\tA\n\t\tN\n\t\t\tT\n\t\t\t\tH\n\t\t\t\t\tO\n\t\t\t\t\t\tS\n\t\t\t\t\t\t\tH\n\t\t\t\t\t\t\tH\n\t\t\t\t\t\tS\n\t\t\t\t\tO\n\t\t\t\tH\n\t\t\tT\n\t\tN\n\tA\nS");
+}
---
+++ name vertical.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("S\na\nn\nt\nh\no\ns\nh\n\nR\na\nj\n");
+}
