3f73d284aa8300c1652cf7649b5779aa4fc88b04
Santhosh Raj Adabala <92911013+Santhosh396@users.noreply.github.com>
2021-10-29 00:04:52 -0700

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	// Variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	// Initialization
+	age = 19;
+	cgpa = 8.0;
+	initial = 'A';
+	
+	// Output
+	printf("Age of the student is %d\n", age);
+	printf("CGPA of the student is %f\n", cgpa);
+	printf("Initial of the student is %c\n", initial);
+}
