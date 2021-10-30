f681778f80bda6c676495915b37848a3ad8242e5
Santhosh Raj Adabala <92911013+Santhosh396@users.noreply.github.com>
2021-10-29 23:48:28 -0700

    Add files via upload

---
+++ Love calculation.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//Variables
+	int Santhosh, Sreshtna, Love;
+	
+	//initialization
+	Santhosh = 50;
+	Sreshtna = 50;
+	
+	//calculation
+	Love = Santhosh+Sreshtna;
+	
+	//output
+	printf("Love of Santhosh %d and Sreshtna %d is %d",Santhosh,Sreshtna,Love);
+}
---
+++ example of variables.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//variables
+	int one, two, three;
+	
+	//initialization
+	one = 499;
+	two = 501;
+	
+	//calculation
+	three = one + two;
+	
+	//output
+	printf("sum of %d and %d is %d",one, two, three);
+}
